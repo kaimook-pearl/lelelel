248147cf652714f64d169a0a94a6c8c44facd736
Unknown <60070132@kmitl.ac.th>
2017-10-05 10:44:05 +0700

    บวก

--- pieeeethonnnn.py
+++ pieeeethonnnn.py
@@ -1,1 +1,9 @@
-def main():
+def cal(num1, num2):
+	summ = summation(num1, num2)
+	subb = subtraction(num1, num2)
+	return subb, summ
+
+def summation(n1, n2):
+	return n1 + n2
+
+cal(int(input()), int(input()))
