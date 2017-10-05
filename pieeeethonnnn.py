dcb07a3a2c07119dea3e4cc4d4174552b16bbdd8
Sand_Nattanicha <60070135@kmitl.ac.th>
2017-10-05 11:06:54 +0700

    fusion

--- pieeeethonnnn.py
+++ pieeeethonnnn.py
@@ -1,12 +1,15 @@
 def cal(num1, num2):
-	summ = summation(num1, num2)
-	subb = subtraction(num1, num2)
-	return subb, summ
+    summ = summation(num1, num2)
+    subb = subtraction(num1, num2)
+    return subb, summ
 
 def summation(n1, n2):
-	return n1 + n2
+    return n1 + n2
 
     def substraction(n1,n2):
         return n1 - n2
+
+    def fusion():
+        print("we wre fusion")
 
     cal(int(input)),int(input())
